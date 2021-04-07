b1cf80d75a61fed4952577f65c26fee0472260d8
Francisco <francisco_2710_javier@hotmail.com>
2021-04-07 12:53:53 -0500

    version 1

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$('#crepasDulces').click(function(){
+	document.getElementById("uno").style.display = "block";
+	document.getElementById("dos").style.display = "none";
+	document.getElementById("tres").style.display = "none";
+	document.getElementById("cuatro").style.display = "none";
+	document.getElementById("cero").style.display = "none";
+});
+
+
+$('#crepasSaladas').click(function(){
+	document.getElementById("uno").style.display = "none";
+	document.getElementById("dos").style.display = "block";
+	document.getElementById("tres").style.display = "none";
+	document.getElementById("cuatro").style.display = "none";
+	document.getElementById("cero").style.display = "none";
+});
+
+
+$('#paninis').click(function(){
+	document.getElementById("uno").style.display = "none";
+	document.getElementById("dos").style.display = "none";
+	document.getElementById("tres").style.display = "block";
+	document.getElementById("cuatro").style.display = "none";
+	document.getElementById("cero").style.display = "none";
+});
+
+$('#cafe').click(function(){
+	document.getElementById("uno").style.display = "none";
+	document.getElementById("dos").style.display = "none";
+	document.getElementById("tres").style.display = "none";
+	document.getElementById("cuatro").style.display = "block";
+	document.getElementById("cero").style.display = "none";
+});
+
+$('#menuCompleto').click(function(){
+	document.getElementById("uno").style.display = "none";
+	document.getElementById("dos").style.display = "none";
+	document.getElementById("tres").style.display = "none";
+	document.getElementById("cuatro").style.display = "none";
+	document.getElementById("cero").style.display = "block";
+});
+
+
+
