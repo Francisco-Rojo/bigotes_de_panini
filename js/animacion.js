529d3e7dd83da4591b663b87965cffac2103dfef
Francisco <francisco_2710_javier@hotmail.com>
2021-05-16 12:34:08 -0500

    actualizacion en animaciones e imagenes de prueba

---
+++ js/animacion.js
@@ -0,0 +1,68 @@
+window.animacion = ScrollReveal();
+
+		animacion.reveal('.navbar', {
+			duration: 1500,
+            origin: 'bottom'
+		});
+
+		animacion.reveal('#animacionFondo', {
+          
+            duration: 1500,
+            delay: 500
+        }); 
+
+         animacion.reveal('#animacionTitulo', {
+            duration: 1500,
+            origin: 'top',
+            distance: '250px'
+        }); 
+
+        animacion.reveal('#animacionSubTitulo', {
+            duration: 1500,
+            origin: 'bottom',
+            distance: '250px'
+        }); 
+
+        animacion.reveal('#animacionMenuTitulo', {
+         	duration: 1500,
+            origin: 'top',
+            distance: '250px',
+            viewFactor: 0.7
+        }); 
+
+        animacion.reveal('#crepasDulces', {
+         	duration: 1500,
+            origin: 'left',
+            distance: '250px',
+            viewFactor: 0.7
+        }); 
+        animacion.reveal('#crepasSaladas', {
+         	duration: 1500,
+            origin: 'right',
+            distance: '250px',
+            viewFactor: 0.7
+        }); 
+        animacion.reveal('#paninis', {
+         	duration: 1500,
+            origin: 'left',
+            distance: '250px',
+            viewFactor: 0.7
+        });
+        animacion.reveal('#cafe', {
+         	duration: 1500,
+            origin: 'right',
+            distance: '250px',
+            viewFactor: 0.7
+        });
+        animacion.reveal('#crepasHelado', {
+         	duration: 1500,
+            origin: 'left',
+            distance: '250px',
+            viewFactor: 0.7
+        });
+        animacion.reveal('#frapes', {
+         	duration: 1500,
+            origin: 'right',
+            distance: '250px',
+            viewFactor: 0.7
+        });
